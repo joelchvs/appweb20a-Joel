298bcebb52e6cdad6b9bc39f918587948280ee23
Joel Chaves <joelchvs93@gmail.com>
2020-08-07 16:44:47 -0600

    eliminando lint

--- js/exc_eslint/multiplication_tables.js
+++
@@ -1,3 +0,0 @@
-console.log('*\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15\t16\t17\t18\t19\t20\t');
-for ( let i = 1; i <= 20; i++ )
-	console.log('%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t', i,i,i*2,i*3,i*4,i*5,i*6,i*7,i*8,i*9, i*10,i*11,i*12,i*13,i*14,i*15,i*16,i*17,i*18,i*19,i*20);
